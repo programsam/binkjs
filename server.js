bb6fd4d8122f13540dfbfeef3862344e2b4ada9a
programsam <programsam@gmail.com>
2015-01-31 18:10:36 -0500

    Make a public directory and import stuff to it.

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+app.use(express.static(__dirname + '/public'));
+
 var server = app.listen(3001, function () {
 
   var host = server.address().address
