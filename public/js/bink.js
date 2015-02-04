c63635ca9ed5d877261dd5d46ddba601eac0362b
programsam <programsam@gmail.com>
2015-02-04 08:32:44 -0500

    x was only from our examples.

--- public/js/bink.js
+++ public/js/bink.js
@@ -10,7 +10,7 @@ function loadRecentJams()
 		var html = "";
 		var result = JSON.parse(data)
   		result.forEach(function (element, index, array) {
-  			mydate = Date.parse(x.substr(0,10)).toString("MM/dd/yyyy")
+  			mydate = Date.parse(element.date.substr(0,10)).toString("MM/dd/yyyy")
   			html += "<div class='item'>"
   			html += "<h1><a href=''>" + mydate + " - " + element.title + "</a></h1>"
   			html += "<div class='quote'>" + element.notes + "</div>"
