f464ce505e788e4a14ab2de0491f608198238665
programsam <programsam@gmail.com>
2015-02-04 08:32:10 -0500

    Use those date libs.

--- public/js/bink.js
+++ public/js/bink.js
@@ -10,8 +10,9 @@ function loadRecentJams()
 		var html = "";
 		var result = JSON.parse(data)
   		result.forEach(function (element, index, array) {
+  			mydate = Date.parse(x.substr(0,10)).toString("MM/dd/yyyy")
   			html += "<div class='item'>"
-  			html += "<h1><a href=''>" + element.date + " - " + element.title + "</a></h1>"
+  			html += "<h1><a href=''>" + mydate + " - " + element.title + "</a></h1>"
   			html += "<div class='quote'>" + element.notes + "</div>"
   			html += "</div>"
   		})
