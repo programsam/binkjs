60fc57feef29e1d72ecb0ece316bbb898e264a24
programsam <programsam@gmail.com>
2015-02-04 08:03:01 -0500

    Use the MySQL connection.

--- server.js
+++ server.js
@@ -7,8 +7,13 @@ function sql() {
 	mysql.createConnection(settings.mysql);
 	return connection.connect();
 }
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
+})
+
+app.get('/recent', function (req, res) {
+	var connection = sql();
 })
 
 app.use(express.static(__dirname + '/public'));
