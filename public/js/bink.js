eb842e323e1d5d3656c7786a7ef09a32509b0723
programsam <programsam@gmail.com>
2015-02-15 17:56:16 -0500

    Give the client a try out.

--- public/js/bink.js
+++ public/js/bink.js
@@ -27,6 +27,16 @@ function loadRecentJams()
 function loadJam(id)
 {
 	$.get( "/jam/" + id, function( data ) {
-		console.log(data)
+		var html = "";
+		var thisjam = JSON.parse(data)
+  		html += "<div class='item'>"
+  		mydate = Date.parse(thisjam.date.substr(0,10)).toString("MM/dd/yyyy")
+		html += "<h1>" + mydate + " - " + thisjam.title + "</a></h1>"
+		if (thisjam.hasOwnProperty("location"))
+		{
+			html += thisjam.location.name
+		}
+		html += "<div class='quote'>" + thisjam.notes + "</div>"
+		html += "</div>"
 	});
 }
