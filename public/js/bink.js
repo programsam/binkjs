6f3ae884993e92959e0d042f6d6dce706af6a2d2
programsam <programsam@gmail.com>
2015-02-15 18:27:57 -0500

    This jam.

--- public/js/bink.js
+++ public/js/bink.js
@@ -35,15 +35,24 @@ function loadRecentJams()
 function loadJam(id)
 {
 	$.get( "/jam/" + id, function( thisjam ) {
+		console.log(thisjam)
 		var html = "";
-  		html += "<div class='item'>"
-  		mydate = Date.parse(thisjam.date.substr(0,10)).toString("MM/dd/yyyy")
-		html += "<h1>" + mydate + " - " + thisjam.title + "</a></h1>"
+		mydate = Date.parse(thisjam.date.substr(0,10)).toString("MM/dd/yyyy")
+		html += "<div class='item'>"
+		html += "<h1><a id='jam" + thisjam.id + "' onclick='loadJam(" + thisjam.id + ")' style='cursor:pointer'>" + mydate + " - " + thisjam.title + "</a></h1>"
+		if (thisjam.hasOwnProperty("band"))
+		{
+			html += thisjam.band.name
+		}
+		if (thisjam.hasOwnProperty("band") && thisjam.hasOwnProperty("location"))
+		{
+			html += " at "
+		}
 		if (thisjam.hasOwnProperty("location"))
 		{
 			html += thisjam.location.name
 		}
-		html += "<div class='quote'>" + thisjam.notes + "</div>"
+		html += "<p /><div class='quote'>" + thisjam.notes + "</div>"
 		html += "</div>"
 		$("#thispage").html(html)
 	});
--- server.js
+++ server.js
@@ -15,7 +15,19 @@ app.get('/', function (req, res) {
 app.get('/jam/:id', function (req, res) {
 	var client = sql();
 	client.query('SELECT * from jams where id = ' + req.params.id, function(err, rows) {
-		result = rows[0]
+		thisjam = rows[0]
+		if (thisjam.bandid != -1)
+		{
+		  	client.query('SELECT * from bands where id = ' + thisjam.bandid, function(err, bands, fields) {
+		  		thisjam.band = bands[0]
+		  	})
+		}
+		if (thisjam.locid != -1)
+		{
+			client.query('SELECT * from locations where id = ' + thisjam.locid, function(err, locations, fields) {
+		  		thisjam.location = locations[0]
+		})
+		}
 		res.set('Content-Type', 'application/json')
 		res.send(result)
 	})
