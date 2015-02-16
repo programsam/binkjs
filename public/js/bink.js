cf13022406b96c2d09769d6db411c10bd47f88e6
Ben Smith <benjsmi@us.ibm.com>
2015-02-16 16:30:30 -0500

    Make the client handle it too.

--- public/js/bink.js
+++ public/js/bink.js
@@ -8,28 +8,34 @@ function loadRecentJams()
 {
 	$.get( "/recent", function( data ) {
 		var html = "";
-		console.log(data)
-  		data.forEach(function (thisjam, index, array) {
-  			mydate = Date.parse(thisjam.date.substr(0,10)).toString("MM/dd/yyyy")
-  			html += "<div class='item'>"
-  			html += "<h1><a id='jam" + thisjam.id + "' onclick='loadJam(" + thisjam.id + ")' style='cursor:pointer'>" + mydate + " - " + thisjam.title + "</a></h1>"
-  			if (thisjam.hasOwnProperty("band"))
-  			{
-  				html += thisjam.band.name
-  			}
-  			if (thisjam.hasOwnProperty("band") && thisjam.hasOwnProperty("location"))
-  			{
-  				html += " at "
-  			}
-  			if (thisjam.hasOwnProperty("location"))
-  			{
-  				html += thisjam.location.name
-  			}
-  			html += "<p /><div class='quote'>" + thisjam.notes + "</div>"
-  			html += "</div>"
-  		})
-  		$("#thispage").html(html)
-	});
+		if (data.error)
+		{
+			alert('We encountered an issue.')
+		}
+		else
+		{
+	  		data.forEach(function (thisjam, index, array) {
+	  			mydate = Date.parse(thisjam.date.substr(0,10)).toString("MM/dd/yyyy")
+	  			html += "<div class='item'>"
+	  			html += "<h1><a id='jam" + thisjam.id + "' onclick='loadJam(" + thisjam.id + ")' style='cursor:pointer'>" + mydate + " - " + thisjam.title + "</a></h1>"
+	  			if (thisjam.hasOwnProperty("band"))
+	  			{
+	  				html += thisjam.band.name
+	  			}
+	  			if (thisjam.hasOwnProperty("band") && thisjam.hasOwnProperty("location"))
+	  			{
+	  				html += " at "
+	  			}
+	  			if (thisjam.hasOwnProperty("location"))
+	  			{
+	  				html += thisjam.location.name
+	  			}
+	  			html += "<p /><div class='quote'>" + thisjam.notes + "</div>"
+	  			html += "</div>"
+	  		})
+	  		$("#thispage").html(html)
+		} //there was no error
+	}); //get
 }
 
 function loadJam(id)
