1e47dbcdffb3f82a51c103e76c0adabee47c989c
Ben Smith <benjsmi@us.ibm.com>
2015-03-03 07:44:42 -0500

    SQL syntax.

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', function (req, res) {
 
 function getItem(field, id)
 {
-	client.query('SELECT * from " + fields + " where id = ' + id, function(err, rows, fields) {
+	client.query("SELECT * from " + field + " where id = " + id, function(err, rows, fields) {
 		if (err) //error while getting the item
 		{
 			console.log("ERROR: " + err)
