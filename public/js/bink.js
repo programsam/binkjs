15628c6cbbc6ac0709ac4110acac4d10852dc55a
programsam <programsam@gmail.com>
2015-03-22 12:55:27 -0400

    Let's just see how it looks first.

--- public/js/bink.js
+++ public/js/bink.js
@@ -39,9 +39,12 @@ function enqueue(setTitle, setPath)
 	console.log("Title: " + setTitle)
 	console.log("Path: " + setPath)
 	var object = { title: setTitle, path: setPath }
-	$("#the-playlist").append("<li><a>" + 
-					object.title + "<span onclick=\"play('" + object.title + "', '" + object.path + "')\"' " +
-					"class='pull-right glyphicon glyphicon-play' aria-hidden='true'></span></a></li>") 
+	$("#the-playlist").append("<li><a>" + object.title + 
+								"<button type='button' class='pull-right btn btn-default' aria-label='Play Button'>"
+	  							"<span class='glyphicon glyphicon-play' aria-hidden='true'></span>"
+								"</button>"
+					//"<span onclick=\"play('" + object.title + "', '" + object.path + "')\"' " +
+					//"class='pull-right glyphicon glyphicon-play' aria-hidden='true'></span></a></li>") 
 	$.ajax({
 	    type: "PUT",
 	    url: "/playlist",
