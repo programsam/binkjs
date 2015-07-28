1961d0f28564115ee065ee6e3c7fb6725ce1528b
Ben Smith <benjsmi@us.ibm.com>
2015-07-28 16:42:53 -0400

    Yup go ahead and load those jams.

--- public/js/bink.js
+++ public/js/bink.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
    $("a#recentButton").click(loadRecentJams)
    $("a#browseButton").click(browse)
-   //loadRecentJams()
+   loadRecentJams()
    
    loadPlaylist()
    
