9974b42a2e3d41a794ec1ab39511492c8efa3e02
Ben Smith <benjsmi@us.ibm.com>
2015-07-29 08:37:12 -0400

    Wrap number setting in a control box.

--- public/js/bink.js
+++ public/js/bink.js
@@ -160,7 +160,7 @@ function browse(size, page)
 		html += "<div id='pages'></div>"
 	  	
 
-	    html += "<div class='btn-group' data-toggle='buttons'>"
+	    html += "<div><div class='btn-group' data-toggle='buttons'>"
 	    for (var j=0;j<nums.length;j++)
 	    {
 	    	html += "<label class='btn btn-primary' id='num" + nums[j] + 
@@ -169,7 +169,7 @@ function browse(size, page)
 		    html += "</label>"
 	    }
 	    
-	    html += "</div>"
+	    html += "</div></div>"
 	    html += "<div id='results'></div>"
 	  	
 	  	$(".main").html(html)
