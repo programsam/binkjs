b20d45706de0165f57b2458097c6e5796c445ffa
Ben Smith <benjsmi@us.ibm.com>
2015-07-31 11:14:35 -0400

    Debug please!

--- public/js/bink.js
+++ public/js/bink.js
@@ -347,17 +347,20 @@ function loadMap()
 		for (var j=0;j<data.length;j++)
 		{
 			var coordinates = new google.maps.LatLng(parseFloat(data[j].lat), parseFloat(data[j].lon));
+			console.log(coordinates)
 			var thismarker = new google.maps.Marker({
 			      position: coordinates,
 			      map: map,
 			      title: data[j].name
 			  });
+			console.log(thismarker)
 			var thiswindow = new google.maps.InfoWindow({
 			      content: "<div id='content" + data[j].id + "'>" + data[j].name + "</div>"
 			  })
-			
+			console.log(thiswindow)
 			google.maps.event.addListener(thismarker, 'click', function() {
 				    thiswindow.open(map,thismarker);
+				    console.log(thismarker)
 			});
 		}
 	})
