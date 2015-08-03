6da079751f8d3c9bacd747b821507ca0c6777dc8
Ben Smith <benjsmi@us.ibm.com>
2015-08-03 17:17:46 -0400

    Add it to the A tag.

--- public/js/bink.js
+++ public/js/bink.js
@@ -65,7 +65,7 @@ function enqueue(setTitle, setPath)
 
 function loadTweets() {
 	clearClasses()
-	$('#main').html("<a class='twitter-timeline' href='https://twitter.com/BINKUpdates' data-widget-id='628310923455909889'>Tweets by @BINKUpdates</a>")
+	$('#main').html("<a class='twitter-timeline' href='https://twitter.com/BINKUpdates' width='100%' height='80%' data-widget-id='628310923455909889'>Tweets by @BINKUpdates</a>")
 	
 	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 }
