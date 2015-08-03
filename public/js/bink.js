bf35cdd863d2ca2d5865ffb03c64c99bc8503e72
Ben Smith <benjsmi@us.ibm.com>
2015-08-03 16:15:55 -0400

    Give that one a shot.

--- public/js/bink.js
+++ public/js/bink.js
@@ -67,26 +67,21 @@ function loadTimeline() {
 	$('#main').addClass('timeline')
 	$.get("/timelineData", function (data) {
 		console.log(data)
-		var data = google.visualization.arrayToDataTable([
-      ['Age', 'Weight'],
-      [ 8,      12],
-      [ 4,      5.5],
-      [ 11,     14],
-      [ 4,      5],
-      [ 3,      3.5],
-      [ 6.5,    7]
-    ]);
-
-	var options = {
-	  title: 'Age vs. Weight comparison',
-	  hAxis: {title: 'Age', minValue: 0, maxValue: 15},
-	  vAxis: {title: 'Weight', minValue: 0, maxValue: 15},
-	  legend: 'none'
-	};
-	
-	var chart = new google.visualization.ScatterChart(document.getElementById('main'));
-	
-	chart.draw(data, options);
+		var data = google.visualization.DataTable();
+		data.addColumn('string', 'Title');
+		data.addColumn('date', 'Date');
+		
+		data.addRows([
+		  [new Date(2008, 5, 3), "Test"]
+		])
+		
+		var options = {
+		  title: 'Jams these Days',
+		  hAxis: {title: 'Age', minValue: 0, maxValue: 15},
+		  vAxis: {title: 'Weight', minValue: 0, maxValue: 15},
+		};
+		var chart = new google.visualization.ScatterChart(document.getElementById('main'));
+		chart.draw(data, options);
 	})
 }
 
