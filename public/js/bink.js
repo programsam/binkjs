2de1f92988b0a16a545d046ce78d578075d4aa4f
programsam <programsam@gmail.com>
2015-08-04 00:08:29 -0400

    Try floating left.

--- public/js/bink.js
+++ public/js/bink.js
@@ -69,7 +69,8 @@ function loadTweets() {
 		var html = ""
 		for (var i=0;i<data.length;i++)
 		{
-			html += "<img src='" + data[i].user.profile_image_url + "' /><strong>" + data[i].user.name + "</strong>"
+			html += "<img src='" + data[i].user.profile_image_url + "' style='float: left' /><strong>&nbsp;" + 
+					data[i].user.name + "</strong>"
 			html += "<font color='gray'>@" + data[i].user.screen_name + "</font> &middot; 23 Aug 2012<br />"
 			html += data[i].text + "<hr />"
 		}
