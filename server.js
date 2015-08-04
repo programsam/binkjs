542dfe6f7ec58d943ba7ffd462f1153d7948114f
programsam <programsam@gmail.com>
2015-08-03 23:51:09 -0400

    Progress...

--- server.js
+++ server.js
@@ -601,7 +601,9 @@ app.get('/api/maps/key', function (req,res) {
 
 app.get('/tweets', function (req, res) {
 	var client = new twitter(settings.twitter)
-	client.get("statuses/user_timeline.json?screen_name=binkupdates", function(error, tweets, response){
+	client.get("statuses/user_timeline",
+	{"screen_name": "binkupdates"}, 
+	function(error, tweets, response){
 	  	if (error)
 	  	{
 	  		console.log("An error occurred retrieving Tweets!")
