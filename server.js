b7a2ffd1dbdd9bfeae23e4146fc0b18b6b2aa10b
Ben Smith <programsam@gmail.com>
2017-05-10 15:35:22 -0400

    Try exporting a function.

--- server.js
+++ server.js
@@ -27,54 +27,7 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
 })
 
-function getJamMusicians(thisjam, overallCallback)
-{
-	var client = sql();
-	var mymusicians = []
-	client.query("SELECT musiciansoncollection.musicianid as musicianid, musiciansoncollection.jamid, " +
-			"musiciansoncollection.instrumentid, " +
-			"musicians.name as musicianname, instruments.name as instrumentname " +
-			"FROM musiciansoncollection, musicians, " +
-			"instruments where instruments.id = musiciansoncollection.instrumentid and musicians.id = " +
-			"musicianid and " +
-			"musiciansoncollection.jamid = ?", [thisjam.id], function(err, musicians, fields) {
-		if (err) //error while getting the item
-		{
-			console.log("ERROR: " + err)
-			client.end()
-			overallCallback()
-		}
-		else
-		{
-			 async.forEach(musicians, function(thismusician, mainCallback) {
-				 var found = false
-				 async.forEach(mymusicians, function(thismymusician, subCallback) {
-				 	 if (thismusician.musicianname == thismymusician.name)
-					 {
-						thismymusician.instruments.push(thismusician.instrumentname)
-						found = true
-					 }
-					 subCallback()
-				 	}, 
-				 	function (err, results) {
-					 if (found == false)
-					 {
-						 var musician = {"name":thismusician.musicianname,
-								 "id": thismusician.musicianid,
-								 "instruments": [thismusician.instrumentname]}
-						 mymusicians.push(musician)
-					 }
-					 mainCallback()
-				  })
-			   },
-			   function(err, results) {
-				 thisjam.musicians = mymusicians
-				 client.end()
-				 overallCallback()
-			   })
-		}
-	})
-}
+require("lib/functions.js")
 
 function getJamTracks(thisjam, overallCallback)
 {
