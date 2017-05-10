256030beb9550cae7d4c796d40cdbef69555bc48
Ben Smith <programsam@gmail.com>
2017-05-10 17:29:00 -0400

    Try that again but better

--- public/js/bink.js
+++ public/js/bink.js
@@ -359,10 +359,8 @@ function loadRecentJams()
 
 funcion binkAlert(alert)
 {
-	$("#main").append('<div class="alert alert-warning alert-dismissible" role="alert">
-	  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-	  <strong>Warning!</strong> Better check yourself, you're not looking too good.
-	</div>
+	$("#thisAlert").html(alert)
+	$("#alertModal").modal('show')
 }
 
 function loadHistoricJams()
