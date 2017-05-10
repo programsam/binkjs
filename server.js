8e9537a9646d56ef51a8f0092839d9d4a4ff3d2a
Ben Smith <programsam@gmail.com>
2017-05-10 15:36:11 -0400

    Try a better path.

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
 })
 
-require("lib/functions")
+require("./lib/functions.js")
 
 function getJamTracks(thisjam, overallCallback)
 {
